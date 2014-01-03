e34e657ea36e8d5006406ed6c12105c3e9bba7ee
Matthew Daly <matthewbdaly@gmail.com>
2014-01-03 15:58:06 +0000

    Added snippets app

--- snippetr/settings.py
+++ snippetr/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'snippets',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,7 +67,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
