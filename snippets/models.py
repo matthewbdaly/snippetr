af85666709c92e1e302df98696d684f9244d7c91
Matthew Daly <matthewbdaly@gmail.com>
2014-01-03 16:13:39 +0000

    Snippet model in place

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Snippet(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now())
